71f518adab0cabfa437309798a5a26f40d8f5648
Vinicius Mendes <viniciusmendes2802@gmail.com>
2024-06-05 14:52:29 -0300

    Soluções para possíveis erros

--- py31.py
+++ py31.py
@@ -1,10 +1,25 @@
-cpf_enviado_usuario = '74682489070'
+import re
+import sys
+
+entrada = input('CPF [746.824.890-70]: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+)
+
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
+
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
+
 nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
-for digito_1 in nove_digitos:
-    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
