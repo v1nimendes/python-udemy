03911f503eb88b7f3cf83ddbe3778b2f036e3e71
Vinicius Mendes <viniciusmendes2802@gmail.com>
2024-06-05 12:29:55 -0300

    Introdução ao try e except para capturar erros (exceptions)

---
+++ py5.py
@@ -0,0 +1,10 @@
+numero_str = input(
+    'Vou dobrar o número que vc digitar: '
+)
+
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
