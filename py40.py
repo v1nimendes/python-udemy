2123ba625a1a7dd3d2a170475bf082bf0348a92a
Vinicius Mendes <viniciusmendes2802@gmail.com>
2024-06-27 13:33:04 -0300

    Introdução ao tipo de dados dict - Dicionários em Python

---
+++ py40.py
@@ -0,0 +1,18 @@
+pessoa = {
+    'nome': 'Vinicius Mendes',
+    'sobrenome': 'de Souza',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ],
+}
+# print(pessoa, type(pessoa))
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+
+print()
+
+for chave in pessoa:
+    print(chave, pessoa[chave])
