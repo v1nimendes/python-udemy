d5e06273539cb4d417183cee062f0a746c332417
Vinicius Mendes <viniciusmendes2802@gmail.com>
2024-07-01 12:01:52 -0300

    Introdução ao tipo set em Python (conjuntos)

--- py46.py
+++ py46.py
@@ -1,3 +1,12 @@
-s1 = {'vini', 1, 2, 3}
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
 
-print(s1)
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+s1 = set()  # vazio
+s1 = {'Luiz', 1, 2, 3}  # com dados
